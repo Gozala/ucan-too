b62d80f74f10a2138559cdd39922cd1510842f4f
Alan Shaw <alan.shaw@protocol.ai>
2023-04-16 18:11:32 +0200

    fix: key rendering

--- resource/main.js
+++ resource/main.js
@@ -11,7 +11,7 @@ export const main = () => {
   </div>
   <div class="dtc v-mid pl3">
     <h1 class="f6 f5-ns fw6 lh-title black mv0">${name === did ? "No Name" : name}</h1>
-    <h2 class="f6 fw4 mt0 mb0 black-60">${did}</h2>
+    <h2 class="f6 fw4 mt0 mb0 black-60 code">${did}</h2>
   </div>
   <div class="dtc v-mid tr">
     <span class="f4 dib h2 w2 br-100 pa2 bg-near-white ba b--black-10 tc lh-copy">${score}</span>
